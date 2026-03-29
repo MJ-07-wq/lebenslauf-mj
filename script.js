40f83111702deb884771fe435934edc564d29d74
MJ-07-wq <jakob.maroufi07@gmail.com>
2026-03-29 15:59:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ document.addEventListener('DOMContentLoaded', function() {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
 });
+
+// Toggle Contact Info
+function toggleContact() {
+    const contactInfo = document.getElementById('contactInfo');
+    if (contactInfo.style.display === 'none') {
+        contactInfo.style.display = 'block';
+    } else {
+        contactInfo.style.display = 'none';
+    }
+}
